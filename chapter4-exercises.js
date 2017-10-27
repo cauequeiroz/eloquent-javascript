ef69b2bfb2252cad3f69d18d151d7aaa625b826f
Caue Queiroz <cauenqueiroz@gmail.com>
2017-10-26 23:11:14 -0200

    finish some exercises

--- chapter4-exercises.js
+++ chapter4-exercises.js
@@ -19,19 +19,50 @@ console.log(range(5, 2, -1));
 console.log(sum(range(1, 10)));
 */
 let range = (start, end, step) => {
-    let result = [];
+    
+    let list = [];
 
-    if ( step ) {
-        let positive = step > 0;
-        
-        for ( let i= start; (positive ? i<=end : i>=end); (positive ? i += step : i -= Math.abs(step)) )
-            result.push(i);
+    if ( start < end ) {
+        for ( let i=start; i<=end; i+= (step || 1) ) list.push(i);
     } else {
-        for ( let i=start; i<=end; i++ )
-            result.push(i);
+        for ( let i=start; i>=end; i-= (Math.abs(step) || 1) ) list.push(i);
     }
 
-    return result;
+    return list;
 }
 
 let sum = list => list.reduce((total, current) => total += current, 0);
+
+/*
+2) Reversing an array
+
+Arrays have a method reverse, which changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method.
+
+Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one is more efficient?
+
+// Your code here.
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+*/
+let reverseArray = list => {
+    let newList = [];
+    
+    for ( let i=0; i<list.length; i++ )
+        newList.push( list[ (list.length-1) - i] )
+
+    return newList;
+}
+
+let reverseArrayInPlace = list => {
+    list = reverseArray(list);
+    console.log(list);
+};
+
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);

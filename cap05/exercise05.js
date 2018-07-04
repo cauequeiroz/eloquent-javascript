8d3596640634c9543d2e5e1d9ae8eaf2347df15c
Caue Queiroz <cauenqueiroz@gmail.com>
2018-07-04 03:00:12 -0300

    finish exercise 5 / cap 5

---
+++ cap05/exercise05.js
@@ -0,0 +1,25 @@
+/*
+  Filter, Map, Reject
+
+  That is not an exercise from Eloquent Javascript Book.
+  Implements filter, map and reject (oposite of filter) functions only
+  with reduce method.
+
+========================================================================= */
+
+const filter = (list, condition) => list.reduce((arr, item) => {
+  return condition(item) ? arr.concat(item) : arr;
+}, []);
+
+const map = (list, transform) => list.reduce((arr, item) => {
+  return arr.concat(transform(item));
+}, []);
+
+const reject = (list, condition) => list.reduce((arr, item) => {
+  return condition(item) ? arr : arr.concat(item);
+}, []);
+
+
+console.log(filter([1, 2, 3, 4, 5, 6], number => number > 3)); // → [4, 5, 6]
+console.log(map([1, 2, 3, 4, 5, 6], number => number * 2)); // → [2, 4, 6, 8, 10, 12]
+console.log(reject([1, 2, 3, 4, 5, 6], number => number > 3)); // → [1, 2, 3]

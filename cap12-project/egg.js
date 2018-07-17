0ab78522f13ae80ae91d3dd9e7686c0a6d2cbf44
Caue Queiroz <cauenqueiroz@gmail.com>
2018-07-17 20:20:52 -0300

    [egg] create language evaluator

--- cap12-project/egg.js
+++ cap12-project/egg.js
@@ -18,8 +18,7 @@ function parseExpression(program) {
   } else {
     throw new SyntaxError("Unexpected syntax: " + program);
   }
-  console.log("EXPR: ", expr)
-  console.log("PROGRAM: ", program.slice(match[0].length))
+
   return parseApply(expr, program.slice(match[0].length));
 }
 
@@ -56,4 +55,33 @@ function parse(program) {
   return expr;
 }
 
-console.log(JSON.stringify(parse("+(a, 10)"), null, 2));
+const specialForms = Object.create(null);
+
+function evaluate(expr, scope) {
+  if (expr.type == "value") {
+    return expr.value;
+
+  } else if (expr.type == "word") {
+    if (expr.name in scope) {
+      return scope[expr.name];
+    } else {
+      throw new ReferenceError(`Undefined binding: ${expr.name}`);
+    }
+
+  } else if (expr.type == "apply") {
+    let { operator, args } = expr;
+
+    if (operator.type == "word" && operator.name in specialForms) {
+      return specialForms[operator.name](expr.args, scope);
+
+    } else {
+      let op = evaluate(operator, scope);
+      
+      if (typeof op == "function") {
+        return op(...args.map(arg => evaluate(arg, scope)));
+      } else {
+        throw new TypeError("Applying a non-function.");
+      }
+    }
+  }
+}

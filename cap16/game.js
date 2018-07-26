8f40bd13e73803df777a2780dd22bcd8e5a1d485
Caue Queiroz <cauenqueiroz@gmail.com>
2018-07-26 18:52:02 -0300

    finish exercise 01 / cap 16

--- cap16/game.js
+++ cap16/game.js
@@ -419,9 +419,20 @@ function runLevel(level, Display) {
 }
 
 async function runGame(plans, Display) {
+  let lives = 3;
+
   for (let level = 0; level < plans.length; ) {
+    console.log(`[Server] Level ${level + 1} - Lives: ${lives}.`);
+
     let status = await runLevel(new Level(plans[level]), Display);
     if (status == "won") level++;
+    if (status == "lost") lives--;
+    
+    if (lives < 0) {
+      console.log('[Server] You died. Restarting game.');
+      level = 0;
+      lives = 3;
+    }
   }
 
   console.log("You've won!");
